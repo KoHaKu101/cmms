74d3146b7efee338b1e01a1f21f3548ce105a9ed
KoHaKu101 <76637663+KoHaKu101@users.noreply.github.com>
2021-06-13 18:54:20 +0700

    Update 2021_01_28_060448_empposition.php

--- database/migrations/2021_01_28_060448_empposition.php
+++ database/migrations/2021_01_28_060448_empposition.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class empposition extends Migration
+class addempposition extends Migration
 {
     /**
      * Run the migrations.
