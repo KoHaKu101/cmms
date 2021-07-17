bbd0c31a0f306d17931e75cb962a217c4616e411
KoHaKu101 <poou8558@gmail.com>
2021-07-17 13:29:06 +0700

    project

--- resources/views/masterlayout/logomasterforuser.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="logo-header" data-background-color="blue">
-
-
-  <div class="nav-toggle">
-    <button class="btn btn-toggle ">
-      <img src="/assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
-    </button>
-  </div>
-</div>
