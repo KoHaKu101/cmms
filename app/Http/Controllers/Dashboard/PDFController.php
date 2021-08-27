40c8f2c6bb4eb7b91977569acf9c346964ffa89e
KoHaKu101 <poou8558@gmail.com>
2021-08-27 09:30:07 +0700

    project

--- app/Http/Controllers/Dashboard/PDFController.php
+++ app/Http/Controllers/Dashboard/PDFController.php
@@ -31,7 +31,7 @@ class PDFController extends Controller
     $this->pdf->AliasNbPages();
     $this->pdf->AddPage(['L',['220', '300'],]);
     $this->pdf->setAutoPageBreak(false);
-    $this->pdf->Rect(5,5,287,200);
+    $this->pdf->Rect(5,5,287,193);
     $this->pdf->header($TYPE);
 
     if ($TYPE == 'DOWNTIME') {
@@ -51,7 +51,7 @@ class PDFController extends Controller
         $REPAIR_SUBSELECT_NAME  = isset($row->REPAIR_SUBSELECT_NAME) ? $row->REPAIR_SUBSELECT_NAME : '-';
         $REPAIR_DETAIL          = isset($row->REPAIR_DETAIL)       ? $row->REPAIR_DETAIL : '-';
         $this->pdf->setX(5);
-        $GET_Y = $this->pdf->getY();
+
         $this->pdf->Row(array(
           $index
           ,$row->MACHINE_CODE
@@ -64,20 +64,10 @@ class PDFController extends Controller
           ,number_format($row->DOWNTIME)
           ,iconv('UTF-8', 'cp874', $CLOSE_BY)
         ));
-        // $this->pdf->Cell(8,  7, $index                                                ,1,0,'C',0);
-        // $this->pdf->Cell(20, 7, $row->MACHINE_CODE                                    ,1,0,'L',0);
-        // $this->pdf->Cell(39, 7, iconv('UTF-8', 'cp874', $MACHINE_NAME)                ,1,0,'L',0);
-        // $this->pdf->Cell(45, 7, iconv('UTF-8', 'cp874', $REPAIR_SUBSELECT_NAME)       ,1,0,'L',0);
-        // $this->pdf->Cell(50, 7, iconv('UTF-8', 'cp874', $REPAIR_DETAIL)               ,1,0,'L',0);
-        // $this->pdf->Cell(25, 7, $INSPECTION_RESULT_TIME                               ,1,0,'C',0);
-        // $this->pdf->Cell(25, 7, $SPAREPART_RESULT_TIME                                ,1,0,'C',0);
-        // $this->pdf->Cell(20, 7, $WORK_RESULT_TIME                                     ,1,0,'C',0);
-        // $this->pdf->Cell(20, 7, number_format($row->DOWNTIME)                         ,1,0,'C',0);
-        // $this->pdf->Cell(35, 7, iconv('UTF-8', 'cp874', $CLOSE_BY)                    ,1,1,'L',0);
-
-        if ($GET_Y == 198) {
+        $GET_Y = $this->pdf->getY();
+        if ($GET_Y > 193) {
           $this->pdf->AddPage(['L','A4',]);
-          $this->pdf->Rect(5,5,287,200);
+          $this->pdf->Rect(5,5,287,193);
           $this->pdf->header($TYPE);
         }
       }
@@ -91,61 +81,81 @@ class PDFController extends Controller
                                             ->where('DOC_MONTH','=',date('n'))->where('CLOSE_STATUS','=',1)->groupBy('MACHINE_CODE')
                                             ->groupBy('MACHINE_UNID')->groupBy('MACHINE_NAME')
                                             ->orderBy('DOWNTIME','DESC')->get();
-
+      $this->pdf->SetWidths(array(8,20,39,45,50,25,25,20,20,35));
+      $this->pdf->SetAligns(array('C','L','L','L','L','C','C','C','C','L'));
       foreach ($DATA_SUM_DOWNTIME as $index => $row){
-
           $REPAIR_SUM   				 = $DATA_REPAIR->where('MACHINE_UNID','=',$row->MACHINE_UNID);
           $ROW_SPAN   					 = count($REPAIR_SUM);
           $number 							 = 1;
           $NUMBER_SUBSELECT_NAME = 1;
           $NUMBER_REPAIR_DETAIL  = 1;
-        foreach ($REPAIR_SUM  as $sub_index => $sub_row){
-            $DOWNTIME = $sub_row->DOWNTIME == 0 ? '-' : $sub_row->DOWNTIME;
-            $this->pdf->setX(5);
-            $GET_Y = $this->pdf->getY();
-            $number_count = $number++;
-            if($number_count == $ROW_SPAN && $number_count == number_format($ROW_SPAN/2)){
-              $this->pdf->Cell(8,  7, $index+1               ,'BR',0,'C',0);
-              $this->pdf->Cell(20, 7, $sub_row->MACHINE_CODE ,'BR',0,'L',0);
-              $this->pdf->Cell(39, 7, iconv('UTF-8', 'cp874', $sub_row->MACHINE_NAME_TH) ,'BR',0,'L',0);
-              $END_DOWNTIME = 1;
-            }elseif ($number_count == $ROW_SPAN) {
-              $this->pdf->Cell(8,  7, ''   ,'BR',0,'C',0);
-              $this->pdf->Cell(20, 7, ''   ,'BR',0,'L',0);
-              $this->pdf->Cell(39, 7, ''   ,'BR',0,'L',0);
-              $END_DOWNTIME = 2;
-            }elseif ($number_count == number_format($ROW_SPAN/2)) {
-              $this->pdf->Cell(8,  7, $index+1               ,'R',0,'C',0);
-              $this->pdf->Cell(20, 7, $sub_row->MACHINE_CODE ,'R',0,'L',0);
-              $this->pdf->Cell(39, 7, iconv('UTF-8', 'cp874', $sub_row->MACHINE_NAME_TH)  ,'R',0,'L',0);
-              $END_DOWNTIME = 3;
-            }else {
-              $this->pdf->Cell(8,  7, ''  ,'R',0,'C',0);
-              $this->pdf->Cell(20, 7, ''  ,'R',0,'L',0);
-              $this->pdf->Cell(39, 7, ''  ,'R',0,'L',0);
-              $END_DOWNTIME = 4;
-            }
+          foreach ($REPAIR_SUM  as $sub_index => $sub_row){
+              $DOWNTIME = $sub_row->DOWNTIME == 0 ? '-' : $sub_row->DOWNTIME;
+              $this->pdf->setX(5);
+              $GET_Y = $this->pdf->getY();
+              $number_count = $number++;
 
-            $this->pdf->Cell(60, 7, iconv('UTF-8', 'cp874', $NUMBER_SUBSELECT_NAME++ .'. '.$sub_row->REPAIR_SUBSELECT_NAME)  ,1,0,'L',0);
-            $this->pdf->Cell(65, 7, iconv('UTF-8', 'cp874', $NUMBER_REPAIR_DETAIL++ .'. '.$sub_row->REPAIR_DETAIL)              ,1,0,'L',0);
-            $this->pdf->Cell(30, 7, iconv('UTF-8', 'cp874', 'ผู้ดำเนินการ')          ,1,0,'L',0);
-            $this->pdf->Cell(30, 7, $END_DOWNTIME     ,1,0,'C',0);
-            if ($END_DOWNTIME == 1) {
-              $this->pdf->Cell(35, 7, $row->DOWNTIME ,'BR',1,'C',0);
-            }elseif ($END_DOWNTIME == 2) {
-              $this->pdf->Cell(35, 7, '' ,'BR',1,'C',0);
-            }elseif ($END_DOWNTIME == 3) {
-              $this->pdf->Cell(35, 7, $row->DOWNTIME ,'R',1,'C',0);
-            }else {
-              $this->pdf->Cell(35, 7, '' ,'R',1,'C',0);
-            }
-            if ($GET_Y == 198) {
-              $this->pdf->AddPage(['L','A4',]);
-              $this->pdf->Rect(5,5,287,200);
-              $this->pdf->header($TYPE);
-            }
 
-        }
+              if($number_count == $ROW_SPAN && $number_count == number_format($ROW_SPAN/2)){
+                // $this->pdf->Cell(8,  7, $index+1               ,'BR',0,'C',0);
+                // $this->pdf->Cell(20, 7, $sub_row->MACHINE_CODE ,'BR',0,'L',0);
+                // $this->pdf->Cell(39, 7, iconv('UTF-8', 'cp874', $sub_row->MACHINE_NAME_TH) ,'BR',0,'L',0);
+              $ONE    =  $index+1;
+              $TWO    =  $sub_row->MACHINE_CODE;
+              $THREE  =  iconv('UTF-8', 'cp874', $sub_row->MACHINE_NAME_TH);
+              }elseif ($number_count == $ROW_SPAN) {
+                // $this->pdf->Cell(8,  7, ''   ,'BR',0,'C',0);
+                // $this->pdf->Cell(20, 7, ''   ,'BR',0,'L',0);
+                // $this->pdf->Cell(39, 7, ''   ,'BR',0,'L',0);
+              $ONE    =  '';
+              $TWO    =  '';
+              $THREE  =  '';
+              }elseif ($number_count == number_format($ROW_SPAN/2)) {
+                // $this->pdf->Cell(8,  7, $index+1               ,'R',0,'C',0);
+                // $this->pdf->Cell(20, 7, $sub_row->MACHINE_CODE ,'R',0,'L',0);
+                // $this->pdf->Cell(39, 7, iconv('UTF-8', 'cp874', $sub_row->MACHINE_NAME_TH)  ,'R',0,'L',0);
+                $ONE    =  $index+1;
+                $TWO    =  $sub_row->MACHINE_CODE;
+                $THREE  =  iconv('UTF-8', 'cp874', $sub_row->MACHINE_NAME_TH);
+              }else {
+                // $this->pdf->Cell(8,  7, ''  ,'R',0,'C',0);
+                // $this->pdf->Cell(20, 7, ''  ,'R',0,'L',0);
+                // $this->pdf->Cell(39, 7, ''  ,'R',0,'L',0);
+                $ONE    =  '';
+                $TWO    =  '';
+                $THREE  =  '';
+              }
+
+              // $this->pdf->Cell(60, 7, iconv('UTF-8', 'cp874', $NUMBER_SUBSELECT_NAME++ .'. '.$sub_row->REPAIR_SUBSELECT_NAME)  ,1,0,'L',0);
+              // $this->pdf->Cell(65, 7, iconv('UTF-8', 'cp874', $NUMBER_REPAIR_DETAIL++ .'. '.$sub_row->REPAIR_DETAIL)              ,1,0,'L',0);
+              // $this->pdf->Cell(30, 7, iconv('UTF-8', 'cp874', 'ผู้ดำเนินการ')          ,1,0,'L',0);
+              // $this->pdf->Cell(30, 7, $DOWNTIME     ,1,0,'C',0);
+               $FOUR  = iconv('UTF-8', 'cp874', $NUMBER_SUBSELECT_NAME++ .'. '.$sub_row->REPAIR_SUBSELECT_NAME);
+               $FIVE  = iconv('UTF-8', 'cp874', $NUMBER_REPAIR_DETAIL++ .'. '.$sub_row->REPAIR_DETAIL);
+               $SIX   = iconv('UTF-8', 'cp874', 'ผู้ดำเนินการ');
+               $SEVEN = $DOWNTIME;
+              if ($number_count == $ROW_SPAN && $number_count == number_format($ROW_SPAN/2)) {
+                // $this->pdf->Cell(35, 7, $row->DOWNTIME ,'BR',1,'C',0);
+                $eight = $row->DOWNTIME;
+              }elseif ($number_count == $ROW_SPAN) {
+                // $this->pdf->Cell(35, 7, '' ,'BR',1,'C',0);
+                $eight = '';
+              }elseif ($number_count == number_format($ROW_SPAN/2)) {
+                // $this->pdf->Cell(35, 7, $row->DOWNTIME ,'R',1,'C',0);
+                $eight = $row->DOWNTIME;
+              }else {
+                // $this->pdf->Cell(35, 7, '' ,'R',1,'C',0);
+                $eight = '';
+              }
+              $this->pdf->Row(array($ONE,$TWO,$THREE,$FOUR,$FIVE,$SIX,$SEVEN,$eight));
+              if ($GET_Y > 198) {
+                $this->pdf->AddPage(['L','A4',]);
+                $this->pdf->Rect(5,5,287,200);
+                $this->pdf->header($TYPE);
+                $this->pdf->setX(5);
+              }
+
+          }
       }
     }
 
