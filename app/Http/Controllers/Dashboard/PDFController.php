21d70c064e9b0915dd95ea82898bdfd12e8e9b83
KoHaKu101 <poou8558@gmail.com>
2021-08-31 11:52:56 +0700

    project

--- app/Http/Controllers/Dashboard/PDFController.php
+++ app/Http/Controllers/Dashboard/PDFController.php
@@ -63,7 +63,7 @@ class PDFController extends Controller
           ,iconv('UTF-8', 'cp874', $CLOSE_BY)
         ));
         $GET_Y = $this->pdf->getY();
-        if ($GET_Y > 180) {
+        if ($GET_Y > 160) {
           $this->pdf->AddPage(['L','A4',]);
           $this->pdf->setX(5);
           $this->pdf->header($TYPE);
